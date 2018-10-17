b5da670c123c3b9d0c1f2076d6136072aea79472
Javier Sújar <javier.sujar@gmail.com>
2018-10-17 16:23:46 +0200

    Fix bug detecting mentions with an existing object that has no mentions

--- tests/test_mentions_signals.py
+++ tests/test_mentions_signals.py
@@ -159,3 +159,37 @@ class TestDjangoMentionSignals(DjangoMentionTestMixins, TestCase):
             self.assertEqual(len(do_mentions_patch.call_args_list), 1)
             self.assertEqual(mock_call_params.get('target'), mention_object)
             self.assertEqual(mock_call_params.get('object_pk'), user_3.pk)
+
+    def test_update_mentions_with_no_previews_mention_at_object(self):
+        # PREPARE DATA
+        description = '<p>{}</p>'.format(fake.text())
+        mention_object = ModelWithCustomDescriptor.objects.create(
+            text=description)
+
+        user_1, user_2 = [mommy.make(
+            get_user_model(),
+            email=fake.email(),
+            first_name=fake.name())
+            for _ in range(2)]
+        user_1_html_pattern = self.DEFAULT_HTML_PATTERN.format(
+            user_1.__class__.__name__, user_1.pk)
+        user_2_html_pattern = self.DEFAULT_HTML_PATTERN.format(
+            user_2.__class__.__name__, user_2.pk)
+
+        # DO ACTION
+        with patch('mentions.wrapper.MentionsWrapper.do_mention') as do_mentions_patch:
+            description = '<p>{} \
+                <a class="class1 class2" {} href="/ecosystem/profile">@{}</a> \
+                <a class="class1 class2" {} href="/ecosystem/profile">@{}</a> \
+                </p>'.format(
+                fake.text(),
+                user_1_html_pattern, user_1.first_name,
+                user_2_html_pattern, user_2.first_name,
+            )
+            mention_object.text = description
+            mention_object.save()
+
+            # ASSERTS
+            _call, mock_call_params = do_mentions_patch.call_args_list[0]
+            self.assertEqual(do_mentions_patch.call_count, 2)
+            self.assertEqual(len(do_mentions_patch.call_args_list), 2)
